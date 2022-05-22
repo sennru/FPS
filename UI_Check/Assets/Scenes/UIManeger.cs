48d50c0ddb3af450cb82f1efdeefdce251837bbd
sennru <childrennano3049@gmail.com>
2022-05-22 13:07:38 +0900

    first commit

---
+++ UI_Check/Assets/Scenes/UIManeger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManeger : MonoBehaviour
+{
+    public GameObject Startview;
+    public GameObject Option;
+    public GameObject Selectmenu;
+    public GameObject Result;
+    public GameObject Game;
+    public GameObject Option_Playgame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveToMenu();
+    }
+
+    // Update is called once per frame
+    public void CurrentPanel()
+    {
+
+    }
+    public void OptionMenu()
+    {
+        Startview.SetActive(false);
+        Option.SetActive(true);
+    }
+    public void MoveToMenu()
+    {
+
+    }
+
+
+}
